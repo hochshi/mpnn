83b58c97c89a1b7ff271571555b8539eb9b81f20
hochshi <hochshi@users.noreply.github.com>
2018-09-04 11:26:53 +0300

    forgot to remove mask

--- models/normed_encoded_basic_model.py
+++ models/normed_encoded_basic_model.py
@@ -65,7 +65,7 @@ class BasicModel(nn.Module):
         bfm = self.bebn(self.be(bfm), adj)
         node_state = afm
         for mf, bn in zip(self.mfs, self.bns):
-            node_state = F.relu(self.uf(self.ma(mf(afm, bfm), adj), node_state, mask), mask)
+            node_state = F.relu(self.uf(self.ma(mf(afm, bfm), adj), node_state, mask))
         return self.of(torch.cat([node_state, afm], dim=-1), mask=mask)
 
     @staticmethod
